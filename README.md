c741902afcad67674e65e06611e846e2278b391e
artic-kuee <artichoke.exe@gmail.com>
2023-01-24 22:21:19 +0900

    improve storage method

--- README.md
+++ README.md
@@ -7,7 +7,9 @@ Buntan utilize [StarPU](https://starpu.gitlabpages.inria.fr/) for scheduling tas
 
 ## Build
 Buntan requires MPIC++, MPIExec, and StarPU installed.
-Since Main-Support utility, which requires MPI Master Slave Support of StarPU will be implemented, StarPU 1.3.10 is recommended.
+Since Main-Support utility requires MPI Master Slave Support of StarPU, StarPU 1.3.10 is recommended.
+In order to utilize MPI utility(runtime_mpi), option "--enable-mpi" is required when building StarPU(On by default)
+In order to utilize Main-Support utility(runtime_ms), option "--enable-mpi-master-slave" is required when building StarPU
 
 ```
 $ mkdir build && cd build
@@ -15,9 +17,21 @@ $ cmake ..
 $ make
 ```
 
-## Options
+## About Executables
+ - client         -generate inputs of circuit and save in ./cloud.data
+ - client_manual  -generate inputs manually
+ - runtime_unode  -evaluate circuit without using mpi and save in ./result.data
+ - runtime_mpi    -evaluate circuit on StarPU MPI Support
+ - runtime_ms     -evaluate circuit on StarPU MPI Master Slave Support
+ - verify         -show result by using secret key
+
+## Compile Options
  - plain_mode     -evaluate in plain operand
  - dump_mode      -show debug info
  - use_simple_FF  -ignore clock and reset input of FF
  - perf_measure   -measure and show duration of procesing
  - use_lvl0       -use TLWElvl0 as a format of coded message
+
+## Execution Options
+ - circuit        -give path of json file which describe the circuit to evaluate(./circuit.json by default)
+ - repetition     -give number of evaluation(once by default)
--- src/runtime_ms.cpp
+++ src/runtime_ms.cpp
@@ -17,24 +17,26 @@
 int world_size;
 TFHEpp::EvalKey ek;
 std::vector<t_val> args_in;
-std::vector<t_val> retvals;
 
 void load_data(void *buffers[], void *cl_arg){
     int numargs, n;
     starpu_codelet_unpack_args(cl_arg, &numargs, &n);
     for(int i = 0; i < numargs; i++){
         *(t_val*)STARPU_VARIABLE_GET_PTR(buffers[i]) = args_in[n * numargs + i];
-#ifdef plain_mode
-        //std::cout << (args_in[n * numargs + i]?"true":"false") << std::endl;
-#endif
     }
 }
 
 void save_result(void *buffers[], void *cl_arg){
-    int numretvals;
-    starpu_codelet_unpack_args(cl_arg, &numretvals);
-    for(int i = 0; i < numretvals; i++){
+    std::vector<t_val> retvals;
+    int numretvals, n;
+    starpu_codelet_unpack_args(cl_arg, &numretvals, &n);
+    for(int i = 0; i < numretvals * n; i++){
         retvals.push_back(*(t_val*)STARPU_VARIABLE_GET_PTR(buffers[i]));
+    }
+    {
+        std::ofstream ofs{"result.data", std::ios::binary};
+        cereal::PortableBinaryOutputArchive ar(ofs);
+        ar(retvals);
     }
 }
 
@@ -84,26 +86,24 @@ int main(int argc, char** argv){
         (&ek)->serialize(ar);
     }
 #endif
-    {
-        inputs = new std::map<std::string, std::pair<int, wire**>>;
-        outputs = new std::map<std::string, std::pair<int, wire**>>; 
-        FFs = new std::map<std::string, std::pair<node*, t_val*>>;
-        ImmTrue = new wire{nullptr, new std::queue<node*>, false, -1}; 
-        ImmFalse = new wire{nullptr, new std::queue<node*>, false, -1};
-        yosys_json_parser(filepath, numwires, inputs, outputs, FFs, ImmTrue, ImmFalse);
-        if(numwires[0] < 0){
-            std::cerr << "frontend failed!" << std::endl;
-            return 1;
-        }
-        if(FFs->size() > 0)init_FFs(FFs);
-        args_in = std::vector<t_val>{};
-        {
-            std::ifstream ifs("./cloud.data", std::ios::binary);
-            cereal::PortableBinaryInputArchive ar(ifs);
-            ar(args_in);
-        }
-        retvals = std::vector<t_val>{};
+    inputs = new std::map<std::string, std::pair<int, wire**>>;
+    outputs = new std::map<std::string, std::pair<int, wire**>>; 
+    FFs = new std::map<std::string, std::pair<node*, t_val*>>;
+    ImmTrue = new wire{nullptr, new std::queue<node*>, false, -1}; 
+    ImmFalse = new wire{nullptr, new std::queue<node*>, false, -1};
+    yosys_json_parser(filepath, numwires, inputs, outputs, FFs, ImmTrue, ImmFalse);
+    if(numwires[0] < 0){
+        std::cerr << "frontend failed!" << std::endl;
+        return 1;
     }
+    if(FFs->size() > 0)init_FFs(FFs);
+    args_in = std::vector<t_val>{};
+    {
+        std::ifstream ifs("./cloud.data", std::ios::binary);
+        cereal::PortableBinaryInputArchive ar(ifs);
+        ar(args_in);
+    }
+
     std::vector<int>* tasks;
 #ifdef use_simple_FF
     int FF_io[numwires[3] * 2];
@@ -137,15 +137,20 @@ int main(int argc, char** argv){
         arg_descrs[i].handle = wire_handles[arg_handle_id[i]];
         arg_descrs[i].mode = STARPU_W;
     }
-    auto retval_descrs = (struct starpu_data_descr*)calloc(numwires[2], sizeof(struct starpu_data_descr));
-    for(int i = 0; i < numwires[2]; i++){
-        retval_descrs[i].handle = wire_handles[retval_handle_id[i]];
+    starpu_data_handle_t retvals[n * numwires[2]];
+    for(int i = 0; i < n * numwires[2]; i++){
+        t_val* ret = (t_val*)calloc(1, sizeof(t_val));
+        starpu_variable_data_register(&retvals[i], STARPU_MAIN_RAM, (uintptr_t)ret, sizeof(t_val));
+    }
+    auto retval_descrs = (struct starpu_data_descr*)calloc(n * numwires[2], sizeof(struct starpu_data_descr));
+    for(int i = 0; i < n * numwires[2]; i++){
+        retval_descrs[i].handle = retvals[i];
         retval_descrs[i].mode = STARPU_R;
     }
-    enum starpu_data_access_mode modes_save[numwires[2]] = {STARPU_R};
+    enum starpu_data_access_mode modes_save[n * numwires[2]] = {STARPU_R};
     struct starpu_codelet save_cl = {
         .cpu_funcs = {save_result},
-        .nbuffers = numwires[2],
+        .nbuffers = n * numwires[2],
         .dyn_modes = modes_save,
     };
 #ifdef perf_measure
@@ -153,7 +158,6 @@ int main(int argc, char** argv){
 #endif
     for(int t = 0; t < n; t++){
         starpu_task_insert(&load_cl,
-            STARPU_EXECUTE_ON_NODE, 0,
             STARPU_VALUE, &(numwires[1]), sizeof(int),
             STARPU_VALUE, &t, sizeof(int),
             STARPU_DATA_MODE_ARRAY, arg_descrs, numwires[1],
@@ -187,19 +191,25 @@ int main(int argc, char** argv){
                 0);
         }
 #endif
-            starpu_task_insert(&save_cl,
-                STARPU_VALUE, &(numwires[2]), sizeof(int),
-                STARPU_DATA_MODE_ARRAY, retval_descrs, numwires[2],
+        for(int i = 0; i < numwires[2]; i++){
+            starpu_task_insert(&copy_cl,
+                STARPU_R, wire_handles[retval_handle_id[i]],
+                STARPU_W, retvals[i + t * numwires[2]],
                 0);
-
+        }
     }
+        starpu_task_insert(&save_cl,
+            STARPU_VALUE, &(numwires[2]), sizeof(int),
+            STARPU_VALUE, &n, sizeof(int),
+            STARPU_DATA_MODE_ARRAY, retval_descrs, n * numwires[2],
+            0);
     
     for(int i = 0; i < numwires[0]; i++){
         starpu_data_unregister(wire_handles[i]);
     }
-    std::ofstream ofs{"result.data", std::ios::binary};
-    cereal::PortableBinaryOutputArchive ar(ofs);
-    ar(retvals);
+    for(int i = 0; i < n * numwires[2]; i++){
+        starpu_data_unregister(retvals[i]);
+    }
     starpu_shutdown();
     #ifdef perf_measure
     shutdown = std::chrono::system_clock::now();
